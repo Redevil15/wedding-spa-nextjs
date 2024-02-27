e00809b410bbb6fc80c22e80a3da4d3eacb9f95b
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-02-27 17:52:49 -0600

    remove env

--- components/confirmacion-component.tsx
+++ components/confirmacion-component.tsx
@@ -2,16 +2,16 @@ import Image from "next/image"
 import { db } from "@/lib/db"
 
 export const ConfirmacionComponent = async () => {
-  const families = await db.family.findMany({
-    include: {
-      invitados: true
-    },
-    orderBy: {
-      nombre_familia: 'asc'
-    }
-  })
+  /*  const families = await db.family.findMany({
+     include: {
+       invitados: true
+     },
+     orderBy: {
+       nombre_familia: 'asc'
+     }
+   }) */
 
-  console.log('FAMILIESSSS: ', families)
+  //console.log('FAMILIESSSS: ', families)
   return (
     <div className="relative w-full px-5 md:px-0 md:max-w-screen-lg mx-auto py-10 md:py-20 bg-cover bg-center">
       <div className="absolute inset-0">
@@ -23,14 +23,6 @@ export const ConfirmacionComponent = async () => {
           className="rounded-lg" // Add any additional styles here
         />
       </div>
-      {families.map((family) => (
-        <div className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden">
-          <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
-          <p className="relative font-semibold text-white">
-            {family.nombre_familia}
-          </p>
-        </div>
-      ))}
 
       Confirmacion component
     </div>
