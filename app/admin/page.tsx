6974248863167dc4e9acbd82bedf6503dd044b88
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-02-28 11:38:54 -0600

    Feat: remove prisma

---
+++ app/admin/page.tsx
@@ -0,0 +1,21 @@
+import { db } from "@/lib/db";
+import { useRouter } from "next/navigation";
+
+const AdminPage = async () => {
+
+  return (
+    <div className="">
+      <div className="flex items-center justify-center pt-4">
+        F & B Admin Page
+      </div>
+      <div className="flex items-center justify-center pt-2">
+        Lista de invitados
+      </div>
+      <div className="flex flex-col space-y-2">
+        
+      </div>
+    </div>
+  )
+}
+
+export default AdminPage;
--- components/confirmacion-component.tsx
+++ components/confirmacion-component.tsx
@@ -11,7 +11,7 @@ export const ConfirmacionComponent = async () => {
     }
   })
 
-  //console.log('FAMILIESSSS: ', families)
+  console.log('FAMILIESSSS: ', families)
   return (
     <div className="relative w-full px-5 md:px-0 md:max-w-screen-lg mx-auto py-10 md:py-20 bg-cover bg-center">
       <div className="absolute inset-0">
