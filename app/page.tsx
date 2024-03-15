191abc6d66ebe1036b2c84d1edbbe437e2201315
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-03-15 15:53:39 -0600

    Fix: icons

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,7 @@ import { MesaRegalos } from "@/components/mesa-regalos";
 import { Navbar } from "@/components/navbar";
 import { PlaceComponent } from "@/components/place-component";
 import { VestimentaComponent } from "@/components/vestimenta-component";
-import dynamic from "next/dynamic";
-import Image from "next/image";
+
 
 export default function Home() {
   const scrollToIds = ['home', 'time&place', 'giftTable', 'confirmation']
